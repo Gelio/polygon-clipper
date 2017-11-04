ec8f795bf47f8a5f58a569e66f2cea72e8d6896d
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-11-04 12:54:58 +0100

    extract LightTypeSelect from InputDataService

--- src/ui/InputDataService.ts
+++ src/ui/InputDataService.ts
@@ -1,18 +1,15 @@
 import { EventAggregator } from 'events/EventAggregator';
-import { NewLightTypeEvent } from 'events/input-data';
-import { RenderEvent } from 'events/RenderEvent';
 
 import { ImageDownloader } from 'services/ImageDownloader';
 import { Service } from 'services/Service';
 
 import { DialogOverlay } from 'ui/components/dialog-overlay/DialogOverlay';
 
-import { LightType } from 'common/LightType';
-
 import {
   BackgroundSelectButton,
   HeightMapSelectButton,
   LightColorSelectButton,
+  LightTypeSelect,
   NormalMapSelectButton
 } from 'ui/components/input-data';
 
@@ -31,29 +28,23 @@ export class InputDataService implements Service {
   private heightMapSelectButton: HeightMapSelectButton;
   private lightColorSelectButton: LightColorSelectButton;
   private normalMapSelectButton: NormalMapSelectButton;
-
-  private lightTypeContainer: HTMLElement;
-  private lightTypeSelect: HTMLSelectElement;
+  private lightTypeSelect: LightTypeSelect;
 
   constructor(dependencies: SerializationServiceDependencies) {
     this.eventAggregator = dependencies.eventAggregator;
     this.imageDownloader = dependencies.imageDownloader;
-
-    this.setupLightTypeSelect();
   }
 
   public init() {
     this.findInputDataContainer();
     this.findDialogOverlay();
-    this.setupButtons();
+    this.setupElements();
 
     this.inputDataContainer.appendChild(this.backgroundSelectButton);
     this.inputDataContainer.appendChild(this.lightColorSelectButton);
     this.inputDataContainer.appendChild(this.normalMapSelectButton);
     this.inputDataContainer.appendChild(this.heightMapSelectButton);
-    this.inputDataContainer.appendChild(this.lightTypeContainer);
-
-    this.lightTypeSelect.addEventListener('change', this.onLightTypeSelectChange);
+    this.inputDataContainer.appendChild(this.lightTypeSelect);
   }
 
   public destroy() {
@@ -61,11 +52,7 @@ export class InputDataService implements Service {
     this.inputDataContainer.removeChild(this.lightColorSelectButton);
     this.inputDataContainer.removeChild(this.normalMapSelectButton);
     this.inputDataContainer.removeChild(this.heightMapSelectButton);
-
-    // tslint:disable-next-line
-    this.inputDataContainer.innerHTML = '';
-
-    this.lightTypeSelect.removeEventListener('change', this.onLightTypeSelectChange);
+    this.inputDataContainer.removeChild(this.lightTypeSelect);
   }
 
   private findInputDataContainer() {
@@ -86,7 +73,7 @@ export class InputDataService implements Service {
     this.dialogOverlay = <DialogOverlay>dialogOverlay;
   }
 
-  private setupButtons() {
+  private setupElements() {
     const { eventAggregator, dialogOverlay, imageDownloader } = this;
 
     const imageSelectButtonDependencies = { eventAggregator, dialogOverlay, imageDownloader };
@@ -96,43 +83,6 @@ export class InputDataService implements Service {
     this.heightMapSelectButton = new HeightMapSelectButton(imageSelectButtonDependencies);
     this.lightColorSelectButton = new LightColorSelectButton(colorSelectButtonDependencies);
     this.normalMapSelectButton = new NormalMapSelectButton(imageSelectButtonDependencies);
-  }
-
-  private setupLightTypeSelect() {
-    this.lightTypeContainer = document.createElement('div');
-
-    const label = document.createElement('label');
-    label.setAttribute('for', 'light-type-select');
-    label.innerText = 'Light type: ';
-    this.lightTypeContainer.appendChild(label);
-
-    this.lightTypeSelect = document.createElement('select');
-    const constantLightTypeOption = document.createElement('option');
-    constantLightTypeOption.innerText = 'Constant ([0, 0, 1])';
-    constantLightTypeOption.value = 'constant';
-
-    const movingLightTypeOption = document.createElement('option');
-    movingLightTypeOption.innerText = 'Moving above the screen';
-    movingLightTypeOption.value = 'moving';
-
-    this.lightTypeSelect.appendChild(constantLightTypeOption);
-    this.lightTypeSelect.appendChild(movingLightTypeOption);
-
-    this.lightTypeContainer.appendChild(this.lightTypeSelect);
-
-    this.onLightTypeSelectChange = this.onLightTypeSelectChange.bind(this);
-  }
-
-  private onLightTypeSelectChange() {
-    const value = this.lightTypeSelect.value;
-    const lightTypes: { [name: string]: LightType } = {
-      constant: LightType.Constant,
-      moving: LightType.Moving
-    };
-
-    const lightType = lightTypes[value];
-
-    this.eventAggregator.dispatchEvent(new NewLightTypeEvent(lightType));
-    this.eventAggregator.dispatchEvent(new RenderEvent());
+    this.lightTypeSelect = new LightTypeSelect({ eventAggregator });
   }
 }
