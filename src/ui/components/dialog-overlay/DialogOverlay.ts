d2ad9929888f46f7fdb7b939a7f4bcd398df8d0d
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-11-04 13:07:31 +0100

    append elements instantly in dialog overlay

--- src/ui/components/dialog-overlay/DialogOverlay.ts
+++ src/ui/components/dialog-overlay/DialogOverlay.ts
@@ -11,18 +11,17 @@ export class DialogOverlay extends HTMLElement {
 
     this.overlayElement = document.createElement('div');
     this.overlayElement.classList.add('overlay');
+    this.appendChild(this.overlayElement);
 
     this.onOverlayClick = this.onOverlayClick.bind(this);
     this.onDialogClose = this.onDialogClose.bind(this);
   }
 
   public connectedCallback() {
-    this.appendChild(this.overlayElement);
     this.overlayElement.addEventListener('click', this.onOverlayClick);
   }
 
   public disconnectedCallback() {
-    this.removeChild(this.overlayElement);
     this.overlayElement.removeEventListener('click', this.onOverlayClick);
   }
 
