7278ab02fc555bf57691938dd779e9f14baa471a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-11-04 09:14:04 +0100

    create barrel index.ts for input data buttons

---
+++ src/ui/components/input-data/index.ts
@@ -0,0 +1,11 @@
+import { BackgroundSelectButton } from 'ui/components/input-data/BackgroundSelectButton';
+import { HeightMapSelectButton } from 'ui/components/input-data/HeightMapSelectButton';
+import { LightColorSelectButton } from 'ui/components/input-data/LightColorSelectButton';
+import { NormalMapSelectButton } from 'ui/components/input-data/NormalMapSelectButton';
+
+export {
+  BackgroundSelectButton,
+  HeightMapSelectButton,
+  LightColorSelectButton,
+  NormalMapSelectButton
+};
